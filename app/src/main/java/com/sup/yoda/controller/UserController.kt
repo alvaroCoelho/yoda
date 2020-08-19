3b42f53b276abf2c77db055a2193408f96b4b1c0
alvaro.coelho <alvarofreitascoelho@gmail.com>
2020-08-19 09:18:24 -0300

    save user

--- app/src/main/java/com/sup/yoda/controller/UserController.kt
+++ app/src/main/java/com/sup/yoda/controller/UserController.kt
@@ -3,12 +3,17 @@ import com.sup.yoda.model.User
 
 class UserController() {
 
+    lateinit var user: User
 
 
-    fun save(user: User){
+    fun save(id:Int,nome:String, email:String){
+       lateinit var userTemp: User
 
+        userTemp.id = id
+        userTemp.nome = nome
+        userTemp.email = email
+
+        user.save(userTemp)
     }
-
-
 
 }
